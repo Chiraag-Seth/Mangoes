5ea366b5742eee7622a27adccdbdf353082d6869
Chiraag-Seth <77068249+Chiraag-Seth@users.noreply.github.com>
2021-02-11 16:12:36 +0000

    Add files via upload

---
+++ rock.js
@@ -0,0 +1,25 @@
+class Rock {
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, 15, 15, options);
+        this.width = 15;
+        this.height = 15;
+        World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+}
+
+
+  
